91e6d3444002ef55e8a97f720b5cf7117a4a2e23
B-Szubert <53540545+B-Szubert@users.noreply.github.com>
2023-04-19 20:10:19 +0200

    Update tomograf_v02.py
    naprawdę się uczę

--- tomograf_v02.py
+++ tomograf_v02.py
@@ -5,5 +5,5 @@ number_1=st.slider("Podaj liczbę skanów",90,720)
 number_2=st.slider("Podaj liczbę dekoderów",90,720)
 number_3=st.slider("Rozpiętość rozmieszczenia dekoderów jako % z długości przekątnej",25,100)
 
-result=st.button("CLick!")
+result=st.button("Rozpocznij ciężkie obliczenia!")
 st.write(result)
