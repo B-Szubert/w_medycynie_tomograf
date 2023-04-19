7f9140ec52fe9af9475b8877bc5476a56d80ca75
B-Szubert <53540545+B-Szubert@users.noreply.github.com>
2023-04-19 20:01:12 +0200

    Update tomograf_v02.py
    pozostałe dwa suwaki

--- tomograf_v02.py
+++ tomograf_v02.py
@@ -3,3 +3,5 @@ st.write("""# My first app
  Hello *world!*""")
 
 number=st.slider("Podaj liczbę skanów",90,720)
+number=st.slider("Podaj liczbę dekoderów",90,720)
+number=st.slider("Rozpiętość rozmieszczenia dekoderów jako % z długości przekątnej",25,100)
