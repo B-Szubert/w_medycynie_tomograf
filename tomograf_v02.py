d9497419825dc8db1cf109f3ef3645a9e3a5d0fd
B-Szubert <53540545+B-Szubert@users.noreply.github.com>
2023-04-19 20:32:34 +0200

    Update tomograf_v02.py
    haloo!!! H A L O!! radio_guziki

--- tomograf_v02.py
+++ tomograf_v02.py
@@ -5,5 +5,20 @@ number_1=st.slider("Podaj liczbę skanów",90,720)
 number_2=st.slider("Podaj liczbę dekoderów",90,720)
 number_3=st.slider("Rozpiętość rozmieszczenia dekoderów jako % z długości przekątnej",25,100)
 
+format_in = st.radio(
+    "Plik jakiego formatu chcesz wczytać?",
+    ('DICOM', 'bit_mapa'))
+format_out = st.radio(
+    "Plik jakiego formatu chcesz uzyskać?",
+    ('DICOM', 'bit_mapa'))
+usrednianie = st.radio(
+    "Jaki sposób uśredniania preferujesz?",
+    ('Brak uśrednienia', 'Najbliższe 4 punkty', 'propozycja pana B...'))
+genre = st.radio(
+    "Czy resultat ma się pokazywać przez cały czas, czy dopiero na końcu?",
+    ('w trakcie obliczeń', 'dopiero na zakończenie'))
+
+
 result=st.button("Rozpocznij ciężkie obliczenia!")
-st.write(result)
+if(result):
+    st.write(result)
