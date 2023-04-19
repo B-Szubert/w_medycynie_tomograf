464b0f0282789bcaf10f5a1bb3dfbe65dc25892b
B-Szubert <53540545+B-Szubert@users.noreply.github.com>
2023-04-19 20:47:46 +0200

    Update tomograf_v02.py
    mózg

--- tomograf_v02.py
+++ tomograf_v02.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.write("""# I'm just learning to write apps in *Streamlit*""")
-st.write('# :otter:')
+st.write('# :brain:')
 
 number_1=st.slider("Podaj liczbę skanów",90,720)
 number_2=st.slider("Podaj liczbę dekoderów",90,720)
